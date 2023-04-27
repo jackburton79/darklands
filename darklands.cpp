c3012f9becd9b14889fadb810c0200154e642466
Jackburton79 <stefano.ceccherini@gmail.com>
2023-04-27 15:34:32 +0200

    Fix crash in case no file is supplied. Added more info

--- darklands.cpp
+++ darklands.cpp
@@ -10,17 +10,23 @@ int main(int argc, char **argv)
 	std::string catalogName = argv[1];
 	std::string fileName = argv[2];
 
+	if (catalogName == "")
+		return 0;
+
 	Catalog catalog(catalogName);
 	catalog.ListEntries();
 	Stream* stream = catalog.GetStream(fileName);
 
-	stream->DumpToFile(fileName.c_str());
+	if (fileName != "")
+		stream->DumpToFile(fileName.c_str());
 	delete stream;
 
 
 	// PIC file:
-	// first 11 bytes are always the same, except for byte at 0x2
-	// 130 060 254 000 035 000 036 000 013 000 040
+	// first 11 bytes are always the same, except for byte at 0x02 and 0x03
+	// sometimes
+	// octal: 130 060 251 000 035 000 036 000 013 000 040
+	// decimal: 88  48 169   0  29   0  30   0  11   0  32
 
 	return 0;
 }
