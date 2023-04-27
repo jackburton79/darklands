65ee266f9e634ce5ec128e233c6af6a53d51fe55
Jackburton79 <stefano.ceccherini@gmail.com>
2023-04-27 19:23:26 +0200

    Import graphics from bgemu

---
+++ graphics/GraphicsEngine.h
@@ -0,0 +1,80 @@
+#ifndef __GRAPHICS_ENGINE_H
+#define __GRAPHICS_ENGINE_H
+
+#include "Bitmap.h"
+#include "GraphicsDefs.h"
+
+#include <vector>
+#include "SDL.h"
+class Bitmap;
+class Listener;
+class GraphicsEngine {
+public:
+	static GraphicsEngine* Get();
+	static bool Initialize();
+	static void Destroy();
+
+
+	enum MASK_VALUES {
+		MASK_NO_MASK = 0,
+		MASK_SHADE = 1,
+		MASK_COMPLETELY = 255
+	};
+
+	static void BlitBitmap(const Bitmap* sourceBitmap, GFX::rect* sourceRect,
+						   Bitmap* destBitmap, GFX::rect* destRect);
+	static void BlitBitmapWithMask(const Bitmap* sourceBitmap, GFX::rect* sourceRect,
+								Bitmap* destBitmap, GFX::rect* destRect,
+								const Bitmap* mask, GFX::rect* maskRect);
+	static void BlitBitmapScaled(const Bitmap* sourceBitmap, GFX::rect* sourceRect,
+							Bitmap* destBitmap, GFX::rect* destRect);
+
+	enum BLITTING_FLAGS {
+		BLITTING_NORMAL = 0,
+		BLITTING_SCALED = 1
+	};
+	void BlitToScreen(const Bitmap* sourceBitmap,
+					  GFX::rect* sourceRect,
+					  GFX::rect* destRect);
+	void BlitToScreen(const Bitmap* sourceBitmap,
+					  const GFX::point& position);
+
+	void SetClipping(const GFX::rect* rect);
+
+	enum VIDEOMODE_FLAGS {
+		VIDEOMODE_WINDOWED = 0,
+		VIDEOMODE_FULLSCREEN = 1
+	};
+	void SetVideoMode(uint16 x, uint16 y, uint16 depth, uint16 flags);
+	void SaveCurrentMode();
+	void RestorePreviousMode();
+
+	GFX::rect ScreenFrame() const;
+	void SetRenderingOffset(const GFX::point& point);
+
+	void SetWindowCaption(const char* caption);
+
+	Bitmap* ScreenBitmap();	
+	void Update();
+	
+	void SetFade(uint16 value);
+
+private:
+	SDL_Window *fSDLWindow;
+	SDL_Renderer *fSDLRenderer;
+	SDL_Texture* fSDLTexture;
+	Bitmap* fScreen;
+	uint16 fFlags;
+	GFX::point fRenderingOffset;
+
+	GFX::rect fOldRect;
+	uint16 fOldDepth;
+	uint16 fOldFlags;
+
+	std::vector<Listener*> fListeners;
+	
+	GraphicsEngine();
+	~GraphicsEngine();
+};
+
+#endif
