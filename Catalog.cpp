25c1f8f822e193673822b286d63f155efab7b58a
JackBurton79 <stefano.ceccherini@gmail.com>
2026-09-13 17:43:58 +0200

    Don't use IGNORE_CASE:
    It fails if we pass a relative path: this is a bug in Streams library

--- Catalog.cpp
+++ Catalog.cpp
@@ -49,7 +49,7 @@ int
 Catalog::SetTo(const std::string& fileName)
 {
 	try {
-		fStream = new FileStream(fileName.c_str(), FileStream::READ_ONLY | FileStream::IGNORE_CASE);
+		fStream = new FileStream(fileName.c_str(), FileStream::READ_ONLY);
 	} catch (...) {
 		std::cerr << "Cannot open " << fileName << std::endl;
 		return -1;
